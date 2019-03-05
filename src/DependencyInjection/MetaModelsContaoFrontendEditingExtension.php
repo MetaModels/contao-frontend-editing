36cf58aa9d8dc9f346b6482dd310617bd47a0870
Mini Model <minimodel@metamodel.me>
2019-03-05 20:44:17 +0100

    Update PHPDoc file headers

--- src/DependencyInjection/MetaModelsContaoFrontendEditingExtension.php
+++ src/DependencyInjection/MetaModelsContaoFrontendEditingExtension.php
@@ -1,20 +1,19 @@
 <?php
 
 /**
- * * This file is part of MetaModels/contao-frontend-editing.
+ * This file is part of MetaModels/contao-frontend-editing.
  *
- * (c) 2016-2017 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage ContaoFrontendEditing
+ * @package    MetaModels/contao-frontend-editing
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
- * @copyright  2016-2017 The MetaModels team.
- * @license    https://github.com/MetaModels/contao-frontend-editing/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/contao-frontend-editing/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
