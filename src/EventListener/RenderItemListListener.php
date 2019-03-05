70b85cedb71a2c1f3a3ab79d9d3b80dd8be7dfb9
Mini Model <minimodel@metamodel.me>
2019-03-05 20:43:57 +0100

    Update PHPDoc file headers

--- src/EventListener/RenderItemListListener.php
+++ src/EventListener/RenderItemListListener.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/contao-frontend-editing.
  *
- * (c) 2012-2018 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,7 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
  * @author     Mini Model <minimodel@metamodel.me>
- * @copyright  2012-2018 The MetaModels team.
+ * @copyright  2012-2019 The MetaModels team.
  * @license    https://github.com/MetaModels/contao-frontend-editing/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
