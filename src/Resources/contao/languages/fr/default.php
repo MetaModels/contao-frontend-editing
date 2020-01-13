a0da63aa8fd0959016452e226c46485a87f8d799
Mini Model <minimodel@metamodel.me>
2020-01-13 18:19:56 +0100

    Updated translations from transifex.

--- src/Resources/contao/languages/fr/default.php
+++ src/Resources/contao/languages/fr/default.php
@@ -1,16 +1,27 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/contao-frontend-editing.
+ *
+ * (c) 2012-2020 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/metamodels/language/fr/
+ * Last-updated: 2020-01-13T18:19:38+01:00
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2016-12-21T22:29:50+01:00
+ * @copyright 2012-2020 The MetaModels team.
+ * @license   https://github.com/MetaModels/contao-frontend-editing/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
 
-$GLOBALS['TL_LANG']['MSC']['metamodel_edit_item'] = 'Éditer l\'élément';
+$GLOBALS['TL_LANG']['MSC']['metamodel_copy_item']   = 'Copier l\'élément';
+$GLOBALS['TL_LANG']['MSC']['metamodel_delete_item'] = 'Supprimer l\'élément';
+$GLOBALS['TL_LANG']['MSC']['metamodel_edit_item']   = 'Éditer l\'élément';
 
