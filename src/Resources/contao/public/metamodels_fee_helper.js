c7aeb2015c11982e456da60e4e3f537334f9dbfb
Ingolf Steinhardt <git@e-spin.de>
2022-02-15 11:12:59 +0100

    Add FEE helper

---
+++ src/Resources/contao/public/metamodels_fee_helper.js
@@ -0,0 +1,66 @@
+/**
+ * This file is part of MetaModels/contao-frontend-editing.
+ *
+ * (c) 2012-2022 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels/contao-frontend-editing
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2022 The MetaModels team.
+ * @license    https://github.com/MetaModels/contao-frontend-editing/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
+ */
+
+(function() {
+    const widgets = document.querySelectorAll('.widget.submitOnChange');
+    if (!widgets.length) {
+        return false;
+    }
+
+    const uuid = location.pathname.split('/').slice(-1).join('');
+    widgets.forEach(function(widget) {
+        let inputs = widget.querySelectorAll('input, select');
+        if (!inputs.length) {
+            return false;
+        }
+
+        inputs.forEach(function(input) {
+            input.addEventListener('change', function() {
+                let hidden = document.createElement('input');
+                hidden.type = 'hidden';
+                hidden.name = 'SUBMIT_TYPE';
+                hidden.value = 'auto';
+
+                sessionStorage.setItem('focus', uuid + ',' + location.hash + ',' + input.id + ',' + window.scrollY);
+
+                input.form.append(hidden);
+                input.form.submit();
+            });
+        });
+    });
+
+    setTimeout(function() {
+        const focus = sessionStorage.getItem('focus');
+        if (!focus) {
+            return false;
+        }
+
+        sessionStorage.removeItem('focus');
+        const focusItems = focus.split(',');
+        if ((uuid !== focusItems[0]) || (location.hash !== focusItems[1])) {
+            return false;
+        }
+
+        const focusElement = document.querySelector('#' + focusItems[2]);
+        if (!focusElement) {
+            return false;
+        }
+        focusElement.focus();
+        document.body.scrollTop = parseInt(focusItems[3]); // For Safari
+        document.documentElement.scrollTop = parseInt(focusItems[3]); // For Chrome, Firefox, IE and Opera
+    }, 50);
+})();
