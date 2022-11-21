f4ef486cb37ebe608af671b86bded49a4de7041c
Mini Model <minimodel@metamodel.me>
2022-11-21 08:46:23 +0100

    Updated translations from transifex.

---
+++ src/Resources/contao/languages/de/tl_nc_notification.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of MetaModels/contao-frontend-editing.
+ *
+ * (c) 2012-2022 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * Last-updated: 2022-11-21T08:46:15+01:00
+ *
+ * @copyright 2012-2022 The MetaModels team.
+ * @license   https://github.com/MetaModels/contao-frontend-editing/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
+ */
+
+
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['metamodels_frontendediting'] = 'MetaModels Frontend-Bearbeitung';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['mm_fe_copy_model']['0']      = 'Datensatz kopiert';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['mm_fe_copy_model']['1']      = 'Dieser Benachrichtigungstyp wird gesendet, wenn ein Datensatz kopiert wurde.';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['mm_fe_create_model']['0']    = 'Datensatz erstellen';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['mm_fe_create_model']['1']    = 'Dieser Benachrichtigungstyp wird gesendet, wenn ein neuer Datensatz erstellt wird.';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['mm_fe_delete_model']['0']    = 'Datensatz gelöscht';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['mm_fe_delete_model']['1']    = 'Dieser Benachrichtigungstyp wird gesendet, wenn ein Datensatz gelöscht wurde.';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['mm_fe_edit_model']['0']      = 'Datensatz bearbeitet';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['mm_fe_edit_model']['1']      = 'Dieser Benachrichtigungstyp wird gesendet, wenn ein Datensatz bearbeitet wurde.';
+
