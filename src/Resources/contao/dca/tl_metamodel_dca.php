13fdb3dda774f8531825227b896c1aa63d2fe4f6
Ingolf Steinhardt <info@e-spin.de>
2023-01-23 12:03:12 +0100

    fix code

--- src/Resources/contao/dca/tl_metamodel_dca.php
+++ src/Resources/contao/dca/tl_metamodel_dca.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/contao-frontend-editing.
  *
- * (c) 2012-202 The MetaModels team.
+ * (c) 2012-2023 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
